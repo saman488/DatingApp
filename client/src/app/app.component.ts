3400c1519f8607dd308c9e1b6e309c31aff1f523
saman <saman1817z@gmail.com>
2023-09-17 15:12:33 +0100

    change title type

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title: any = 'Dating app';
+  title  = 'Dating app';
   users: any;
 
   constructor(private http: HttpClient){ }
